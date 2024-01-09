b6be2ef250194627a3d404d47941775c4764c62d
JPcookie290 <julia.paier@yahoo.com>
2024-01-09 10:16:10 +0100

    start graph.ts

--- dataStructure/graph/graph.js
+++ dataStructure/graph/graph.js
@@ -16,21 +16,31 @@ class Graph {
         if (this.adjacencyList.get(vertex1) && this.adjacencyList.get(vertex2)) {
             //this.adjacencyList.get(vertex1)!.splice(this.adjacencyList.get(vertex1)!.indexOf(vertex2), 1);
             //this.adjacencyList.get(vertex2)!.splice(this.adjacencyList.get(vertex2)!.indexOf(vertex1), 1);
+            //Chritoph:
             this.adjacencyList.set(vertex1, this.adjacencyList.get(vertex1).filter((a) => a !== vertex2));
             this.adjacencyList.set(vertex2, this.adjacencyList.get(vertex2).filter((a) => a !== vertex1));
         }
     }
     removeVertex(vertex) {
-        var _a, _b;
-        //console.log(this.adjacencyList.has(vertex));
-        //console.log(this.adjacencyList.get(vertex));
-        //this.adjacencyList.get(vertex)!.forEach(element => {
-        //    console.log(this.adjacencyList.get(element));
-        //});
-        while ((_a = this.adjacencyList.get(vertex)) === null || _a === void 0 ? void 0 : _a.length) {
-            this.removeEdge(vertex, (_b = this.adjacencyList.get(vertex)) === null || _b === void 0 ? void 0 : _b.pop());
-        }
+        this.adjacencyList.get(vertex).forEach(element => {
+            this.removeEdge(element, vertex);
+        });
         this.adjacencyList.delete(vertex);
+        //Chritoph:
+        //while(this.adjacencyList.get(vertex)?.length){
+        //    this.removeEdge(vertex, this.adjacencyList.get(vertex)?.pop() as T)
+        //}
+    }
+    depthFirst(startVertex) {
+        //wenn iterativ
+        const stack = [startVertex]; //push, pop
+        const result = [];
+        const visted = {};
+        visted[startVertex] = true;
+        visted.Berlin;
+        //do something
+        while (stack.length) { }
+        return result;
     }
 }
 //Test
@@ -43,9 +53,9 @@ graph.addEdge("Berlin", "Wien");
 graph.addEdge("Berlin", "London");
 graph.addEdge("Berlin", "Barcelona");
 graph.addEdge("Wien", "Barcelona");
-console.log(graph.adjacencyList);
-graph.removeEdge("Berlin", "Wien");
-console.log(graph.adjacencyList);
-graph.removeVertex("London");
+//console.log(graph.adjacencyList);
+//graph.removeEdge("Berlin", "Wien");
+//console.log(graph.adjacencyList);
+//graph.removeVertex("London");
 console.log(graph.adjacencyList);
 //# sourceMappingURL=graph.js.map
--- dataStructure/graph/graph.ts
+++ dataStructure/graph/graph.ts
@@ -26,25 +26,38 @@ class Graph<T> {
         if(this.adjacencyList.get(vertex1) && this.adjacencyList.get(vertex2)) {
             //this.adjacencyList.get(vertex1)!.splice(this.adjacencyList.get(vertex1)!.indexOf(vertex2), 1);
             //this.adjacencyList.get(vertex2)!.splice(this.adjacencyList.get(vertex2)!.indexOf(vertex1), 1);
-            this.adjacencyList.set(vertex1, this.adjacencyList.get(vertex1)!.filter((a:T) => a !== vertex2));
-            this.adjacencyList.set(vertex2, this.adjacencyList.get(vertex2)!.filter((a:T) => a !== vertex1));
+
+            //Chritoph:
+            this.adjacencyList.set(vertex1, this.adjacencyList.get(vertex1)!.filter((a: T) => a !== vertex2));
+            this.adjacencyList.set(vertex2, this.adjacencyList.get(vertex2)!.filter((a: T) => a !== vertex1));
         }
     }
 
     removeVertex(vertex: T){
-        //console.log(this.adjacencyList.has(vertex));
-        //console.log(this.adjacencyList.get(vertex));
-        //this.adjacencyList.get(vertex)!.forEach(element => {
-        //    console.log(this.adjacencyList.get(element));
-        //});
-        while(this.adjacencyList.get(vertex)?.length){
-            this.removeEdge(vertex, this.adjacencyList.get(vertex)?.pop() as T)
-        }
+        this.adjacencyList.get(vertex)!.forEach(element => {
+            this.removeEdge(element, vertex)
+        });
+        
         this.adjacencyList.delete(vertex)
+        
+        //Chritoph:
+        //while(this.adjacencyList.get(vertex)?.length){
+        //    this.removeEdge(vertex, this.adjacencyList.get(vertex)?.pop() as T)
+        //}
     }
 
+    depthFirst(startVertex: T): T[]{
+        //wenn iterativ
+        const stack = [startVertex]; //push, pop
+        const result: T[] = [];
+        const visted: {[key: string]: boolean} = {};
+        visted[startVertex as string] = true;
+        visted.Berlin
+        //do something
+        while(stack.length) {}        
 
-
+        return result
+    }
 }
 
 //Test
@@ -57,11 +70,13 @@ graph.addEdge("Berlin", "Wien");
 graph.addEdge("Berlin", "London");
 graph.addEdge("Berlin", "Barcelona");
 graph.addEdge("Wien", "Barcelona");
+//console.log(graph.adjacencyList);
+//graph.removeEdge("Berlin", "Wien");
+//console.log(graph.adjacencyList);
+//graph.removeVertex("London");
 console.log(graph.adjacencyList);
-graph.removeEdge("Berlin", "Wien");
-console.log(graph.adjacencyList);
-graph.removeVertex("London");
-console.log(graph.adjacencyList);
+
+
 
 
 
